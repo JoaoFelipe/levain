13139c996280420bb1f28faeeeb3f3a674118119
Joao Felipe Nicolaci Pimentel <joao.pimentel@bndes.gov.br>
2026-07-22 14:33:31 -0300

    #161 Replace git pull command to make it run faster

--- src/lib/repository/git_repository.ts
+++ src/lib/repository/git_repository.ts
@@ -30,7 +30,7 @@ export default class GitRepository extends AbstractRepository {
   async init(): Promise<void> {
     if (existsSync(this.localDir)) {
       try {
-        await this.gitUtils.pull(this.localDir);
+        await this.gitUtils.update(this.localDir);
       } catch (error) {
         log.info(`Ignoring GIT error and moving on. Error:${error}`);
       }
